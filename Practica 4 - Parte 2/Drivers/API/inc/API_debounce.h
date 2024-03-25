d7b6cfd0dbb2b2b8d7761cd205655c8aa2718760
Leandro <alba_canalla@hotmail.com>
2024-03-24 23:57:55 -0300

    Practica 4 Parte 2 Terminada

---
+++ Practica 4 - Parte 2/Drivers/API/inc/API_debounce.h
@@ -0,0 +1,19 @@
+/*
+ * API_debounce.h
+ *
+ *  Created on: Mar 23, 2024
+ *      Author: lalba
+ */
+
+#ifndef API_INC_API_DEBOUNCE_H_
+#define API_INC_API_DEBOUNCE_H_
+
+#include <stdbool.h>
+#include "stm32f4xx_nucleo_144.h" 	/* <- BSP include */
+#include "API_delay.h"
+
+void debounceFSM_init(void);
+void debounceFSM_update(void);
+bool_t readKey(void);
+
+#endif /* API_INC_API_DEBOUNCE_H_ */
